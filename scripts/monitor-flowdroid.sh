75cb4d7fdc392b5366a3fee491f044c8bad7f03c
Louis Narmour <lnarmour@rams.colostate.edu>
2020-10-17 09:48:33 -0600

    monitor flowdroid visual changes

--- scripts/monitor-flowdroid.sh
+++ scripts/monitor-flowdroid.sh
@@ -7,16 +7,17 @@ function gather {
   for l in log/flowdroid/*.log; 
   do 
     app=`echo $l | sed 's~.*/\(.*\)~\1~'`;
-    echo "- $(cat $l | grep 'Found .* leaks' | sed 's~.*\(Found .* leaks\).*~\1~')"; 
+    if [[ -n "$DEBUG" ]]; then debug_app="  ($app)"; else debug_app=""; fi;
+    echo "- $(cat $l | grep 'Found .* leaks' | sed "s~.*\(Found .* leaks\).*~\1$debug_app~")"; 
   done;
 }
 
-function gather_sort {
+function match_tree {
   echo ""
   echo ""
-  gather | sort
+  gather
   echo ""
   echo ""
 }
 
-paste -d ' ' <(tree log) <(gather_sort)
+paste -d ' ' <(tree log) <(match_tree)
